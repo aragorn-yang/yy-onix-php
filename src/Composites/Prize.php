95aff5e867b4235de25fd7bff8fdf78e68f63564
李峰 <75320364@qq.com>
2020-01-05 23:34:37 +0800

    add Prize

--- src/Composites/Prize.php
+++ src/Composites/Prize.php
@@ -2,10 +2,23 @@
 
 namespace AragornYang\Onix\Composites;
 
+use AragornYang\Onix\CodeInList;
+use AragornYang\Onix\CodeLists\CodeList41PrizeOrAwardAchievement;
+use AragornYang\Onix\CodeLists\CodeList91CountryCodeISO31661;
+
 class Prize extends Composite
 {
     /** @var string */
     protected $prizeName = '';
+
+    /** @var string */
+    protected $prizeYear = '';
+
+    /** @var CodeInList */
+    protected $prizeCode;
+
+    /** @var CodeInList */
+    protected $prizeCountry;
 
     public function getPrizeName(): string
     {
@@ -15,5 +28,45 @@ class Prize extends Composite
     public function setPrizeName(string $prizeName): void
     {
         $this->prizeName = $prizeName;
+    }
+
+    public function setPrizeYear(string $value): void
+    {
+        $this->prizeYear = $value;
+    }
+
+    public function getPrizeYear(): string
+    {
+        return $this->prizeYear;
+    }
+
+    public function setPrizeCode(string $code): void
+    {
+        $this->prizeCode = new CodeInList(CodeList41PrizeOrAwardAchievement::class, $code);
+    }
+
+    public function getPrizeCode(): string
+    {
+        return $this->prizeCode ? $this->prizeCode->code() : '';
+    }
+
+    public function getPrizeCodeDesc(): string
+    {
+        return $this->prizeCode ? $this->prizeCode->desc() : '';
+    }
+
+    public function setPrizeCountry(string $code): void
+    {
+        $this->prizeCountry = new CodeInList(CodeList91CountryCodeISO31661::class, $code);
+    }
+
+    public function getPrizeCountry(): string
+    {
+        return $this->prizeCountry ? $this->prizeCountry->code() : '';
+    }
+
+    public function getPrizeCountryDesc(): string
+    {
+        return $this->prizeCountry ? $this->prizeCountry->desc() : '';
     }
 }
