612e920acafb285faad82bc3d01f598e7f7ca6fc
李峰 <75320364@qq.com>
2020-01-06 18:30:23 +0800

    通过XmlReader解释XML文件，测试下来速度更快。（后面会尽量再优化一下）

---
+++ tests/Features/V21/StreamBookParser2ForTag21SampleOnixFileTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AragornYang\Onix\Tests\Features\V21;
+
+use AragornYang\Onix\BookParser;
+use AragornYang\Onix\Composites\Product;
+use AragornYang\Onix\StreamBookParser2;
+use AragornYang\Onix\Tests\Features\BookParserForSampleOnixFileTest;
+
+class StreamBookParser2ForTag21SampleOnixFileTest extends BookParserForSampleOnixFileTest
+{
+    use SampleOnixFile21, UseV21Handler;
+
+    /** @var BookParser */
+    protected static $parser;
+    protected static $parsed = false;
+    /** @var array|Product[] */
+    protected static $products;
+    /** @var Product */
+    protected $product;
+
+    protected function getParser(): BookParser
+    {
+        return new StreamBookParser2;
+    }
+
+    protected function getPath(): string
+    {
+        return __DIR__ . '/samples/sample.v.2.1.tag.onx';
+    }
+}
