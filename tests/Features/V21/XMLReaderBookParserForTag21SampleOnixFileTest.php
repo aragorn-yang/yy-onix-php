6160be5ee7660317fd897dade2e8147eac08189a
李峰 <75320364@qq.com>
2020-01-08 16:47:08 +0800

    XMLReaderBookParse

---
+++ tests/Features/V21/XMLReaderBookParserForTag21SampleOnixFileTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AragornYang\Onix\Tests\Features\V21;
+
+use AragornYang\Onix\BookParser;
+use AragornYang\Onix\Composites\Product;
+use AragornYang\Onix\XMLReaderBookParser;
+use AragornYang\Onix\Tests\Features\BookParserForSampleOnixFileTest;
+
+class XMLReaderBookParserForTag21SampleOnixFileTest extends BookParserForSampleOnixFileTest
+{
+    use SampleOnixFile21, UseV21Handler;
+
+    /** @var BookParser */
+    protected static $parser;
+    protected static $parsed = false;
+    /** @var array|Product[] */
+    protected static $products;
+    /** @var Product */
+    protected $product;
+
+    protected function getParser(): BookParser
+    {
+        return new XMLReaderBookParser;
+    }
+
+    protected function getPath(): string
+    {
+        return __DIR__ . '/samples/sample.v.2.1.tag.onx';
+    }
+}
