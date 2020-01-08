5eb989317834d50a171d0b5e818880bb7b751d35
李峰 <75320364@qq.com>
2020-01-08 15:08:40 +0800

    优化继承的方法

--- src/StreamBookParser2.php
+++ src/StreamBookParser2.php
@@ -3,17 +3,17 @@ namespace AragornYang\Onix;
 
 class StreamBookParser2 extends BookParser
 {
-    public function parseString(string $xmlString): void
+    public function parseString(string $contents): void
     {
         $xml = new \XMLReader();
-        $xml->xml($xmlString, 'UTF-8');
+        $xml->xml($contents, 'UTF-8');
         $this->parseStream($xml);
     }
 
-    public function parseFile(string $xmlFile): void
+    public function parseFile(string $file): void
     {
         $xml = new \XMLReader();
-        $xml->open($xmlFile, 'UTF-8');
+        $xml->open($file, 'UTF-8');
         $this->parseStream($xml);
     }
 
@@ -29,14 +29,14 @@ class StreamBookParser2 extends BookParser
                 $xmlDom = $xml->readOuterXml();
                 ($this->productHandler)(Onix::getInstance()->buildProduct(simplexml_load_string($xmlDom)));
             } elseif (in_array(ucfirst($xml->name), ['ONIXMessage', 'ONIXmessage'])) {
-                $this->parOnixEdition($xml);
+                $this->parseOnixEdition($xml);
             } else {
                 continue;
             }
         }
     }
 
-    protected function parOnixEdition($xml): void
+    protected function parseOnixEdition(\XMLReader $xml): void
     {
         $onix = Onix::getInstance();
 
