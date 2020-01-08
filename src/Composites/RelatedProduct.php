bc2d9a486bfbaed644f353bbfae3591cfdc77d0f
李峰 <75320364@qq.com>
2020-01-09 01:04:53 +0800

    add RelatedProduct

--- src/Composites/RelatedProduct.php
+++ src/Composites/RelatedProduct.php
@@ -6,12 +6,17 @@ use AragornYang\Onix\CodeInList;
 use AragornYang\Onix\CodeLists\CodeList51ProductRelation;
 use AragornYang\Onix\ProductFeatures\HasProductForm;
 use AragornYang\Onix\ProductFeatures\HasProductIdentifiers;
+use AragornYang\Onix\CodeLists\CodeList10EpublicationType;
 
 class RelatedProduct extends Composite
 {
     use HasProductForm, HasProductIdentifiers;
+
     /** @var CodeInList */
     protected $relationCode;
+
+    /** @var CodeInList */
+    protected $epubType;
 
     public function getRelationCode(): string
     {
@@ -26,5 +31,20 @@ class RelatedProduct extends Composite
     public function setRelationCode(string $code): void
     {
         $this->relationCode = new CodeInList(CodeList51ProductRelation::class, $code);
+    }
+
+    public function getEpubType(): string
+    {
+        return $this->epubType ? $this->epubType->code() : '';
+    }
+
+    public function getEpubTypeDesc(): string
+    {
+        return $this->epubType ? $this->epubType->desc() : '';
+    }
+
+    public function setEpubType(string $code): void
+    {
+        $this->epubType = new CodeInList(CodeList10EpublicationType::class, $code);
     }
 }
